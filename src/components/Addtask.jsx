31e912690266e44e1cffcf69e8b2ef93ec6caf5d
kash!sh <singhkashish14203@gmail.com>
2024-06-14 20:59:44 +0530

    ..

--- src/components/Addtask.jsx
+++ src/components/Addtask.jsx
@@ -120,7 +120,7 @@ setNote({...note, [e.target.name]: e.target.value})
           </div>
 
           <div>
-          <button type="submit"  className="btn -mt-7 text-white bg-purple-800 hover:bg-purple-500 focus:ring-4 focus:ring-purple-200 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none" disabled={note.title.length < 5 || note.description.length < 5}>Add Task</button>
+          <button type="submit"  className="btn -mt-7 text-white bg-purple-800 hover:bg-purple-500 focus:ring-4 focus:ring-purple-200 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none z-50" disabled={note.title.length < 2 || note.description.length < 5}>Add Task</button>
           </div>
       </form>
   </div>
